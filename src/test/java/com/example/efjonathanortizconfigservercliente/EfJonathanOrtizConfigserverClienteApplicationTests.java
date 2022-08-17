d5bd15f9ef89723ebcf9c70f1012ee2db9eedeb2
JhonasPaul <91173212+JhonasPaul@users.noreply.github.com>
2022-08-16 22:00:15 -0500

    evaluacion final

---
+++ src/test/java/com/example/efjonathanortizconfigservercliente/EfJonathanOrtizConfigserverClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.efjonathanortizconfigservercliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfJonathanOrtizConfigserverClienteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
